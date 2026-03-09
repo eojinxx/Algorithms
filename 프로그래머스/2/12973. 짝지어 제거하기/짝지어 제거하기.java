2d49e7ea1d395d38312cd8ca957bdca133b85e91
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-03-09 21:28:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.20 ms, Memory: 66.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution
+{   
+    public int solution(String s)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) stack.push(c);
+            else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty() ? 1: 0;
+    }
+    
+}
