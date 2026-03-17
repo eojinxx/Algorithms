13b5b1fe00c41cbe7b812a949b82926c09bd326a
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-03-17 20:36:14 +0900

    [level 2] Title: 오픈채팅방, Time: 141.84 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -4,39 +4,29 @@ import java.util.*;
 // return: 최종적으로 방을 개설한 사람이 보게되는 문자열 배열
 class Solution {
     public String[] solution(String[] record) {
-        HashMap<String, String> idToNick = new HashMap<>();
-        // Id와 Command 보관
-        List<String[]> logs = new ArrayList<>();
-
-
-        for (String line : record) {
-            String[] info = line.split(" ");
-            String command = info[0];
-            String uid = info[1];
-            
-            if (command.equals("Enter") || command.equals("Change")) {
-                idToNick.put(uid, info[2]);
-            }
-            
-            if (command.equals("Enter") || command.equals("Leave")) {
-                logs.add(new String[] {uid, command});
+        HashMap<String, String> msg = new HashMap<>();
+        msg.put("Enter", "님이 들어왔습니다.");
+        msg.put("Leave", "님이 나갔습니다.");
+        
+        HashMap<String, String> uid = new HashMap<>();
+        
+        for (String s : record) {
+            String cmd[] = s.split(" ");
+            if (cmd.length == 3) {
+                uid.put(cmd[1], cmd[2]);
             }
         }
-
-        String[] answer = new String[logs.size()];
-        for (int i = 0; i < logs.size(); i++) {
-            String uid = logs.get(i)[0];
-            String command = logs.get(i)[1];
-            String nickname = idToNick.get(uid);
+        
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for (String s : record) {
+            String cmd[] = s.split(" ");
             
-            if (command.equals("Enter")) {
-                answer[i] = nickname + "님이 들어왔습니다.";
-            } else {
-                answer[i] = nickname + "님이 나갔습니다.";
+            if (msg.containsKey(cmd[0])) {
+                answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
             }
         }
-
-
-        return answer;
+        
+        return answer.toArray(String[]::new);
     }
 }
