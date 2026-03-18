3e47277c8385d58a2f4351c95aa4c1030b813d7f
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-03-18 17:27:23 +0900

    [level 1] Title: 신고 결과 받기, Time: 153.09 ms, Memory: 182 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    // return: 각 유저가 받은 정지 피드백 메일 수를 담은 배열
+    public int[] solution(String[] id_list, String[] report, int k) {
+        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
+        HashMap<String, Integer> feedBackCnt = new HashMap<>();
+        
+        
+        // 신고 받은 유저 목록을 저장
+        for (String s : report) {
+            String[] tmp = s.split(" ");
+            String reporter = tmp[0];
+            String respondent = tmp[1];
+            
+            if (!reportMap.containsKey(respondent)) {
+                reportMap.put(respondent, new HashSet<>());
+            }
+            reportMap.get(respondent).add(reporter);
+        }
+        
+        // 피드백 횟수를 유저 별로 저장
+        for (String s : id_list) {
+            if (reportMap.containsKey(s) && reportMap.get(s).size() >= k) {
+                HashSet<String> tmp = reportMap.get(s);
+                for (String ss : tmp) {
+                    feedBackCnt.put(ss, feedBackCnt.getOrDefault(ss, 0) + 1);
+                }
+            }
+        }
+        
+        int[] answer = new int[id_list.length];
+        
+        // 피드백 횟수를 배열에 저장
+        for (int i = 0; i < id_list.length; i++) {
+            String reporter = id_list[i];
+            if (feedBackCnt.containsKey(reporter)) {
+                answer[i] = feedBackCnt.get(reporter);
+            } 
+        }
+        
+        return answer;
+    }
+}
