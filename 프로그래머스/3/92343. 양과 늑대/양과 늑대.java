3cb9405024db4024fdcbb210d9b5f2ceb97f5c52
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-03-25 18:35:02 +0900

    [level 3] Title: 양과 늑대, Time: 27.20 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -2,60 +2,53 @@ import java.util.*;
 
 class Solution {
     private static class Info {
-        int node, sheep, wolf;
-        HashSet<Integer> visited;
+        int node, wolf, sheep;
+        HashSet<Integer> canVisit;
 
-        public Info(int node, int sheep, int wolf, HashSet<Integer> visited) {
+        Info(int node, int wolf, int sheep, HashSet<Integer> canVisit) {
             this.node = node;
-            this.sheep = sheep;
             this.wolf = wolf;
-            this.visited = visited;
+            this.sheep = sheep;
+            this.canVisit = canVisit;
         }
     }
 
-    private static ArrayList<Integer>[] tree; // 인접 리스트
-    private static void buildTree(int[] info, int[][] edges) {
-        tree = new ArrayList[info.length];
+    private static ArrayList<Integer>[] tree;
+    public static int solution(int[] info, int[][] edges) {
+        int answer = Integer.MIN_VALUE;
 
-        for (int i = 0; i < info.length; i++) {
+        tree = new ArrayList[info.length];
+        for (int i = 0; i < tree.length; i++) {
             tree[i] = new ArrayList<>();
         }
 
         for (int[] edge : edges) {
             tree[edge[0]].add(edge[1]);
         }
-    }
-    
-    public int solution(int[] info, int[][] edges) {
-        buildTree(info, edges);
-        int answer = 0;
-        
+
         ArrayDeque<Info> queue = new ArrayDeque<>();
-        queue.add(new Info(0, 1, 0, new HashSet<>()));
-        
+        queue.add(new Info(0, 0, 1, new HashSet<>()));
+
         while (!queue.isEmpty()) {
             Info now = queue.poll();
-            
             answer = Math.max(answer, now.sheep);
-            // 방문한 노드 집합에 현재 노드의 이웃 노드 추가
-            now.visited.addAll(tree[now.node]);
             
-            // 인접한 노드들의 대해 탐색
-            for (int next : now.visited) {
-                // 기존 해시셋의 데이터를 복사하고 현재 방문한 정점을 해시셋에서 제거
-                HashSet<Integer> set = new HashSet<>(now.visited);
+            now.canVisit.addAll(tree[now.node]);
+            
+            for (int next : now.canVisit) {
+                HashSet<Integer> set = new HashSet<>(now.canVisit);
                 set.remove(next);
                 
                 if (info[next] == 1) {
                     if (now.sheep > now.wolf + 1) {
-                        queue.add(new Info(next, now.sheep, now.wolf + 1, set));
+                        queue.offer(new Info(next, now.wolf + 1, now.sheep, set));
                     }
                 } else {
-                    queue.add(new Info(next, now.sheep + 1, now.wolf, set));
+                    queue.offer(new Info(next, now.wolf, now.sheep + 1, set));
                 }
             }
         }
-        
         return answer;
+
     }
 }
