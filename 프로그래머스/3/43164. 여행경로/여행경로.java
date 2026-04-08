391076987db04c7cf59ab89c5e4576e649c2c2ed
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-04-08 14:30:31 +0900

    [level 3] Title: 여행경로, Time: 0.49 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    
+    // 막다른 노드인 경우 생각
+    private static void dfs(String now) {
+        while (map.containsKey(now) && !map.get(now).isEmpty()) {
+            dfs(map.get(now).poll());    
+        }
+        
+        answer.add(now);
+    }
+    private static HashMap<String, PriorityQueue<String>> map;
+    private static ArrayList<String> answer;
+    public String[] solution(String[][] tickets) {
+        map = new HashMap<>();
+        answer = new ArrayList<>();
+        
+        for (String[] ticket : tickets) {
+            if (!map.containsKey(ticket[0])) {
+                map.put(ticket[0], new PriorityQueue<>());
+            }
+            
+            map.get(ticket[0]).offer(ticket[1]);
+        }
+        
+        
+        
+        dfs("ICN");
+        String[] arr = new String[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+                arr[answer.size() - 1 - i] = answer.get(i);
+        }
+        return arr;
+    }
+}
