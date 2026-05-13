fcdef584351bf80a2f083702cad942adbcb59f0d
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-13 20:44:41 +0900

    [level 2] Title: 튜플, Time: 40.81 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-       ArrayList<Integer> ans = new ArrayList<>();
-
+        ArrayList<Integer> resultList = new ArrayList<>();
         String str = s.substring(2, s.length() - 2);
         String[] arr = str.split("\\},\\{");
         Arrays.sort(arr, Comparator.comparing(String::length));
         
-        ans.add(Integer.parseInt(arr[0]));
-        
+        resultList.add(Integer.parseInt(arr[0]));
         for (int i = 1; i < arr.length; i++) {
             String[] tmp = arr[i].split(",");
-            for (int j = 0; j < tmp.length; j++) {
-                if (!ans.contains(Integer.parseInt(tmp[j]))) {
-                    ans.add(Integer.parseInt(tmp[j]));
+            for (String a : tmp) {
+                if (!resultList.contains(Integer.parseInt(a))) {
+                    resultList.add(Integer.parseInt(a));
                 }
             }
         }
         
-        return ans.stream().mapToInt(Integer::intValue).toArray();
+        
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
