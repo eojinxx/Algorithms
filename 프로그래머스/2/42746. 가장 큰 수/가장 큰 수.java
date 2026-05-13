785a3521429a0feef208f4ec1512d30140b38491
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-13 20:24:58 +0900

    [level 2] Title: 가장 큰 수, Time: 1671.57 ms, Memory: 371 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,18 +1,14 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-   
     public String solution(int[] numbers) {
-        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).sorted((o1, o2) -> o1.charAt(0) == o2.charAt(0) ? (o2 + o1).compareTo(o1 + o2) : Character.compare(o2.charAt(0), o1.charAt(0))).toArray(String[]::new);
+       String[] arr = Arrays.stream(numbers).mapToObj(i -> String.valueOf(i)).sorted((o1, o2) -> o1.charAt(0) == o2.charAt(0) ? (o2 + o1).compareTo(o1 + o2): o2.compareTo(o1)).toArray(String[]::new);
         
         String ans = "";
-        for (int i = 0; i < arr.length; i++) {
-            ans += arr[i];
+        for (String s : arr) {
+            ans += s;
         }
         
         return ans.charAt(0) == '0' ? "0" : ans;
     }
-    
-    
 }
