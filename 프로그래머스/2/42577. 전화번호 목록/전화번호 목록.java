24409c6fd6bdf5cd6c0edf3000f4443c25e3372f
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-13 20:48:12 +0900

    [level 2] Title: 전화번호 목록, Time: 209.04 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,16 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, Comparator.naturalOrder());
-        
+        Arrays.sort(phone_book);
         for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i].length() > phone_book[i + 1].length()) continue;
-            int len = phone_book[i].length();
-            String s = phone_book[i + 1].substring(0, len);
-            
-            if (phone_book[i].equals(s)) return false;
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
         }
-        
         return true;
     }
 }
