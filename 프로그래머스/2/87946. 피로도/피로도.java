930ed1d852d0c3ddedf46aa884c742dd02f72fdf
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-13 19:33:51 +0900

    [level 2] Title: 피로도, Time: 1.62 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,23 @@
-import java.util.*;
-
 class Solution {
-    private static int result;
+    private static int ans;
     private static boolean[] check;
     
-    private static void dfs(int k, int[][] dungeons, int cnt) {
+    public static void dfs(int k, int[][] dungeons, int cnt) {
+        
         for (int i = 0; i < dungeons.length; i++) {
-            if (dungeons[i][0] <= k && !check[i]) {
+            if (k >= dungeons[i][0] && !check[i]) {
                 check[i] = true;
                 dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                ans = Math.max(ans, cnt + 1);
                 check[i] = false;
             }
         }
-        result = Math.max(result, cnt);
     }
     
     public int solution(int k, int[][] dungeons) {
-        check = new boolean[dungeons.length];
+        check  = new boolean[dungeons.length];
         dfs(k, dungeons, 0);
+        return ans;
         
-        return result;
     }
 }
