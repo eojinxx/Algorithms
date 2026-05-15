6c1713bbf998126ac411a2378943a47b1468fa5b
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-15 15:24:21 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.37 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    private static int[] dp;
+
+    public int solution(int n) {
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
+        }
+        
+        return dp[n];
+    }
+}
