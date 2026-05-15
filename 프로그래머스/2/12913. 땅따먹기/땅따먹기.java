d8a42f8ffb880f5c6971b6212ecbafb3d14f0f9d
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-05-15 17:37:54 +0900

    [level 2] Title: 땅따먹기, Time: 11.59 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    int solution(int[][] land) {
+        int n = land.length;
+        int[][] dp = new int[n][4];
+        
+        for (int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+        
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 4; j++) {
+                for (int k = 0; k < 4; k++) {
+                    if (j != k) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - 1][k] + land[i][j]);
+                    }
+                }
+            }
+        }
+        
+ 
+        return Arrays.stream(dp[n - 1]).max().getAsInt();
+    }
+}
