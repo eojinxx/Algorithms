4b76ecdb6c8c295426c033ec696fc5b4494cd0dd
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-09-22 20:04:21 +0900

    update

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -7,11 +7,10 @@ class Solution
         int r = board.length, c = board[0].length;
         
         for (int i = 1; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (i - 1 < 0 || j - 1 < 0) continue;
-                if (board[i][j] == 0 || board[i - 1][j] == 0 || board[i - 1][j - 1] == 0 || board[i][j - 1] == 0) continue;
+            for (int j = 1; j < c; j++) {
+                if (board[i][j] == 1)
                 
-                board[i][j] = Math.min(Math.min(board[i - 1][j], board[i - 1][j - 1]), board[i][j - 1]) + 1;
+                    board[i][j] = Math.min(Math.min(board[i - 1][j], board[i - 1][j - 1]), board[i][j - 1]) + 1;
             }
         }
         
