29274500d64fe4cff5aaaa0420364f4c8b372533
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-09-26 19:50:28 +0900

    [level 1] Title: 신고 결과 받기, Time: 118.49 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,53 +1,33 @@
 import java.util.*;
 
 class Solution {
-    // return: 각 유저가 받은 정지 피드백 메일 수를 담은 배열
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
-        HashMap<String, Integer> feedBackCnt = new HashMap<>();
+        HashMap<String, HashSet<String>> map = new HashMap<>();
         
-        
-        // 신고 받은 유저 목록을 저장
         for (String s : report) {
             String[] tmp = s.split(" ");
             String reporter = tmp[0];
-            String respondent = tmp[1];
+            String reportee = tmp[1];
             
-            if (!reportMap.containsKey(respondent)) {
-                reportMap.put(respondent, new HashSet<>());
+            if (!map.containsKey(reportee)) {
+                map.put(reportee, new HashSet<>());
             }
-            reportMap.get(respondent).add(reporter);
+            
+            map.get(reportee).add(reporter);
         }
         
-        // 피드백 횟수를 유저 별로 저장
-        for (String s : id_list) {
-            if (reportMap.containsKey(s) && reportMap.get(s).size() >= k) {
-                HashSet<String> tmp = reportMap.get(s);
-                for (String ss : tmp) {
-                    feedBackCnt.put(ss, feedBackCnt.getOrDefault(ss, 0) + 1);
+        int[] ans = new int[id_list.length];
+        
+        map.entrySet().stream()
+            .filter(o -> o.getValue().size() >= k)
+            .forEach(o -> {
+                for (int i = 0; i < id_list.length; i++) {
+                    if (o.getValue().contains(id_list[i])) {
+                        ans[i] += 1;
+                    }
                 }
-            }
-        }
-        /**
-        reportedMap.entrySet().stream().map(Map.Entry<String, HashSet<String>>::getValue).forEach(set -> {
-            if (set.size() >= k) {
-                for (String s : set) {
-                    count.put(s, count.getOrDefault(s, 0) + 1);
-                }
-            }
-        });
-        */
+            });
         
-        int[] answer = new int[id_list.length];
-        
-        // 피드백 횟수를 배열에 저장
-        for (int i = 0; i < id_list.length; i++) {
-            String reporter = id_list[i];
-            if (feedBackCnt.containsKey(reporter)) {
-                answer[i] = feedBackCnt.get(reporter);
-            } 
-        }
-        
-        return answer;
+        return ans;
     }
 }
