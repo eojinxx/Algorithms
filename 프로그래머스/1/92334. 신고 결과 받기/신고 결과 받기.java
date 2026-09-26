60a7a625e19af46880548c494512fe319a0aa5ee
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-09-26 20:07:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 94.45 ms, Memory: 173 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,27 +3,30 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         HashMap<String, HashSet<String>> map = new HashMap<>();
-        
+
         for (String s : report) {
             String[] tmp = s.split(" ");
             String reporter = tmp[0];
             String reportee = tmp[1];
-            
+
             map.computeIfAbsent(reportee, x -> new HashSet<>()).add(reporter);
         }
-        
+
+        Map<String, Integer> index = new HashMap<>();
+        for (int i = 0; i < id_list.length; i++) {
+            index.put(id_list[i], i);
+        }
+
         int[] ans = new int[id_list.length];
-        
-        map.entrySet().stream()
-            .filter(o -> o.getValue().size() >= k)
-            .forEach(o -> {
-                for (int i = 0; i < id_list.length; i++) {
-                    if (o.getValue().contains(id_list[i])) {
-                        ans[i] += 1;
-                    }
-                }
-            });
-        
+
+        for (HashSet<String> reporters : map.values()) {
+            if (reporters.size() < k) continue;
+
+            for (String reporter : reporters) {
+                ans[index.get(reporter)]++;
+            }
+        }
+
         return ans;
     }
 }
