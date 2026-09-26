5a82ce85f642cdf6567e72c1a0fbd6c105dbd54a
Eo Jin Kim <88206611+eojinxx@users.noreply.github.com>
2026-09-26 17:33:52 +0900

    [level 2] Title: 오픈채팅방, Time: 99.51 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,32 +1,25 @@
 import java.util.*;
 
-// 중복 닉네임 허용
-// return: 최종적으로 방을 개설한 사람이 보게되는 문자열 배열
 class Solution {
     public String[] solution(String[] record) {
-        HashMap<String, String> msg = new HashMap<>();
-        msg.put("Enter", "님이 들어왔습니다.");
-        msg.put("Leave", "님이 나갔습니다.");
-        
-        HashMap<String, String> uid = new HashMap<>();
+        ArrayList<String> ans = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
         
         for (String s : record) {
-            String cmd[] = s.split(" ");
-            if (cmd.length == 3) {
-                uid.put(cmd[1], cmd[2]);
+            String[] token = s.split(" ");
+            if (!token[0].equals("Leave")) {
+                map.put(token[1], token[2]);
             }
         }
         
-        ArrayList<String> answer = new ArrayList<>();
-        
         for (String s : record) {
-            String cmd[] = s.split(" ");
-            
-            if (msg.containsKey(cmd[0])) {
-                answer.add(uid.get(cmd[1]) + msg.get(cmd[0]));
+            String[] token = s.split(" ");
+            if (!token[0].equals("Change")) {
+                String verb = token[0].equals("Enter") ? "들어왔습니다." : "나갔습니다.";
+                ans.add(map.get(token[1]) + "님이 " + verb);
             }
         }
         
-        return answer.toArray(String[]::new);
+        return ans.toArray(String[]::new);
     }
 }
